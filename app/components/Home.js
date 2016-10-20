931a1f5c0e5a917f8045e177a466a129ffee3091
Martinez Urrea <jume@gft.com>
2016-10-20 16:45:55 +0200

    minor changes

--- app/components/Home.js
+++ app/components/Home.js
@@ -9,6 +9,7 @@
 			<div className='jumbotron col-sm-12 text-center'>  
 				<h1>Weather App</h1>
 				<p className='lead'>Sun is Shining</p>
+				<h2>Enter a city and state</h2>
 				
 			</div>
 		 )
